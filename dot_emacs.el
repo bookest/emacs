3093291c0691ec51fffcfa891c40f860fc54669e
Christopher Grim <christopher.grim@gmail.com>
2007-08-07 00:33:06 +0000

    Add unicode helpers for prettying up font lock mode.
    Add pretty lambdas to python-mode.

--- dot_emacs.el
+++ dot_emacs.el
@@ -341,6 +341,71 @@ The current selected frame is moved if FRAME is NIL."
   (cjg-set-frame-position frame -1 0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; unicode prettiness
+(defvar unicode-symbol-alist
+  '((left-arrow . 8592)
+    (up-arrow . 8593)
+    (right-arrow . 8594)
+    (down-arrow . 8595)
+    (double-vertical-bar . #X2551)
+    (equal . #X003d)
+    (not-equal . #X2260)
+    (identical . #X2261)
+    (not-identical . #X2262)
+    (less-than . #X003c)
+    (greater-than . #X003e)
+    (less-than-or-equal-to . #X2264)
+    (greater-than-or-equal-to . #X2265)
+    (logical-and . #X2227)
+    (logical-or . #X2228)
+    (logical-neg . #X00AC)
+    (nil . #X2205)
+    (horizontal-ellipsis . #X2026)
+    (double-exclamation . #X203C)
+    (prime . #X2032)
+    (double-prime . #X2033)
+    (for-all . #X2200)
+    (there-exists . #X2203)
+    (element-of . #X2208)
+    (square-root . #X221A)
+    (squared . #X00B2)
+    (cubed . #X00B3)
+    (lambda . #X03BB)
+    (alpha . #X03B1)
+    (beta . #X03B2)
+    (gamma . #X03B3)
+    (delta . #X03B4)))
+
+(defun unicode-symbol (name)
+  "Translate a symbolic name for a Unicode character -- e.g., LEFT-ARROW
+or GREATER-THAN into an actual Unicode character code. "
+  (let ((char-code (cdr (assoc name unicode-symbol-alist))))
+    (if (null char-code)
+        (error "Unknown character name: '%s'" name)
+      (decode-char 'ucs char-code))))
+
+
+(defun substitute-pattern-with-unicode (pattern symbol)
+  "Add a font lock hook to replace the matched part of PATTERN with the 
+Unicode symbol SYMBOL."
+  (interactive)
+  (when window-system
+    (font-lock-add-keywords
+     nil
+     `((,pattern (0 (progn
+                      (compose-region (match-beginning 1)
+                                      (match-end 1)
+                                      ,(unicode-symbol symbol))
+                      nil)))))))
+  
+(defun substitute-patterns-with-unicode (patterns)
+  "Call SUBSTITUTE-PATTERN-WITH-UNICODE repeatedly."
+  (mapcar #'(lambda (x)
+              (substitute-pattern-with-unicode (car x)
+                                               (cdr x)))
+          patterns))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; mode specific configs
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -604,7 +669,8 @@ Makefile or makefile exist in the current directory."
     (cjg-enable 'abbrev-mode
                 'outline-minor-mode)
     (turn-on-eldoc-mode)
-    (flyspell-prog-mode))
+    (flyspell-prog-mode)
+    (substitute-pattern-with-unicode "\\<\\(lambda\\>\\)" 'lambda))
   
   (defun pylint ()
     "Run pylint against the file visited by the current buffer.
