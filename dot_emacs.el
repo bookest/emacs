a08008aa67eb6dc9f902fe6d245e783c1bc8cd60
Christopher Grim <christopher.grim@gmail.com>
2008-04-06 18:10:44 -0400

    use cjg-with-flymake-tempfile in cjg-shell-flymake-init

--- dot_emacs.el
+++ dot_emacs.el
@@ -714,11 +714,7 @@ This is a modified version of something I stole from perlmonks."
 
   (when (require 'flymake t)
     (defun cjg-shell-flymake-init ()
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                         'flymake-create-temp-inplace))
-             (local-file (file-relative-name
-                          temp-file
-                          (file-name-directory buffer-file-name))))
+      (cjg-with-flymake-tempfile local-file
         `("bash" ("-n" ,local-file))))
     
     (defvar cjg-flymake-shell-err-line-pattern
